fa7e708957416c75e3d1c965cd85df4faaea4ae8
moeinebrahimzade89 <moein.ebrahimzade.89@gmail.com>
2026-07-10 12:06:03 +0330

    Add a comment

--- max.py
+++ max.py
@@ -1,6 +1,8 @@
+#The bottom three lines take numbers from the input.
 a = int(input("number1:"))
 b = int(input("number2:"))
 c = int(input("number3:"))
+#The lower bets check which number is greater or equal.
 if a == b == c :
     print("Equal")
 elif a > b:
